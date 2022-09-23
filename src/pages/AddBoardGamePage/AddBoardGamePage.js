fd34bd20c008b9738a2cb346965e2f1489673a0d
Michael Mifsud-Sweeney <michaelmifsudsweeney@gmail.com>
2022-09-22 21:47:49 -0400

    add controlled components

--- src/pages/AddBoardGamePage/AddBoardGamePage.js
+++ src/pages/AddBoardGamePage/AddBoardGamePage.js
@@ -7,9 +7,29 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
+import uuid4 from "uuid4";
+
+const CURRENT_USER_ID = process.env.REACT_APP_CURRENT_USER_ID;
 function AddBoardGamePage() {
+  //ok so I've got the server calling when you submit
+  //now I just need to:
+  //get all the forms to be filled
+  //add any forms I need personally
+  //generate the final object to send
+  //rewire the api to just post whatever it gets
+  //axios post it
+  let [bgName, setbgName] = useState("")
+  let [bgDescription, setbgDescription] = useState("")
+  let [bgMinDuration, setbgMinDuration] = useState("")
+  let [bgMaxDuration, setbgMaxDuration] = useState("")
+  let [bgMinPlayers, setbgMinPlayers] = useState("")
+  let [bgMaxPlayers, setbgMaxPlayers] = useState("")
+  let [bgImage, setbgImage] = useState("")
+  let [bgCategory, setbgCategory] = useState("")
   let [bgCondition, setbgCondition] = useState("")
+
   let [gameDataFromServer, setGameDataFromServer] = useState({})
+
   const [options, setOptions] = useState([
     { label: 'Pulp Fiction', id: 2 },
   ]);
@@ -49,7 +69,25 @@ function AddBoardGamePage() {
 
 
   let submitNewBoardGameHandler = (e) => {
+    e.preventDefault()
+    console.log("submitFiring")
+    let newGame = {
+      "gameId": uuid4(),
+      "gameName": bgName,
+      "gameDescription": bgDescription,
+      "gameDuration": `${bgMinDuration}-${bgMaxDuration}`,
+      "renterId": "",
+      "gameMinPlayers": bgMinPlayers,
+      "gameMaxPlayers": bgMaxPlayers,
+      "image": bgImage,
+      "gameCategory": "mavSOM8vjH",
+      "ownerId": CURRENT_USER_ID,
+      "gameAvailability": "AVAILABLE",
+      "gameCondition": bgCondition,
+      "gameReviews": []
+    }
 
+    console.log(newGame)
   }
 
   let selectBoardGameHandler = (e) => {
@@ -57,17 +95,36 @@ function AddBoardGamePage() {
     console.log("onSubmit", typeof (e.target.innerHTML))
     let selectedGameOption = options.find((option) => option.label === e.target.innerHTML)
     if (e.target.innerText !== undefined) {
-      console.log("this is fired")
+      // console.log("this is fired")
       axios.get(`https://api.boardgameatlas.com/api/search?ids=${selectedGameOption.id}&client_id=JLBr5npPhV`)
         .then((res) => {
-          console.log("returnedData is:", res.data.games[0])
+          // console.log("returnedData is:", res.data.games[0])
           setGameDataFromServer(res.data.games[0])
+          setbgName(res.data.games[0].name)
+          setbgDescription(res.data.games[0].description)
+          setbgMinDuration(res.data.games[0].min_playtime)
+          setbgMaxDuration(res.data.games[0].max_playtime)
+          setbgMinPlayers(res.data.games[0].min_players)
+          setbgMaxPlayers(res.data.games[0].max_players)
+          setbgImage(res.data.games[0].image_url)
+      
+          // console.log()
+         
+          
         })
     }
-
-    // console.log("selectedGameId",selectedGameId)
-
   }
+
+  const onbgNameChange = (e) => setbgName(e.target.value);
+  const onbgDescription = (e) => setbgDescription(e.target.value);
+  const onbgMinDuration = (e) => setbgMinDuration(e.target.value);
+  const onbgMaxDuration = (e) => setbgMaxDuration(e.target.value);
+  const onbgMinPlayersChange = (e) => setbgMinPlayers(e.target.value);
+  const onbgMaxPlayersChange = (e) => setbgMaxPlayers(e.target.value);
+  const onbgImageChange = (e) => setbgImage(e.target.value);
+  const onbgCategoryChange = (e) => setbgCategory(e.target.value);
+  
+
   return (<>
     <div className="addBoardGameContainer">
       <form onSubmit={(e) => submitNewBoardGameHandler(e)}>
@@ -81,7 +138,7 @@ function AddBoardGamePage() {
           }}
           id="combo-box-demo"
           options={options}
-          renderInput={(params) => <TextField {...params} label="Movie" />}
+          renderInput={(params) => <TextField {...params} label="Board Game" />}
         />
         <FormControl fullWidth>
           <InputLabel id="demo-simple-select-label">Condition</InputLabel>
@@ -98,15 +155,18 @@ function AddBoardGamePage() {
             <MenuItem value="Honestly Bad">Honestly Bad</MenuItem>
           </Select>
         </FormControl>
+
+        <TextField id="outlined-name" label="bgName" value={bgName} onChange={onbgNameChange} />
+        <TextField id="outlined-name" label="bgDescription" multiline value={bgDescription} onChange={onbgDescription} />
+        <TextField id="outlined-name" label="bgMinDuration" value={bgMinDuration} onChange={onbgMinDuration} />
+        <TextField id="outlined-name" label="bgMaxDuration" value={bgMaxDuration} onChange={onbgMaxDuration} />
+        <TextField id="outlined-name" label="bgMinPlayers" value={bgMinPlayers} onChange={onbgMinPlayersChange} />
+        <TextField id="outlined-name" label="bgMaxPlayers" value={bgMaxPlayers} onChange={onbgMaxPlayersChange} />
+        <TextField id="outlined-name" label="bgImage" value={bgImage} onChange={onbgImageChange} />
+        <TextField id="outlined-name" label="bgCategory" value={bgCategory} onChange={onbgCategoryChange} />
+        
         {/* <input type="text" name="itemSelected" className="item-details__item-name-input" value={inventoryName} placeholder='Item Name' onChange={onNameChange} /> */}
         <button>Submit!</button>
-        <p>{gameDataFromServer.id}</p>
-        <p>{gameDataFromServer.name}</p>
-        <p>{gameDataFromServer.description}</p>
-        <p>{gameDataFromServer.max_players}</p>
-        <p>{gameDataFromServer.max_playtime}</p>
-        <p>{gameDataFromServer.min_players}</p>
-        <p>{gameDataFromServer.min_playtime}</p>
       </form>
     </div>
   </>
