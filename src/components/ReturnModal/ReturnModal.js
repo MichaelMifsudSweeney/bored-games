a243c385627620a04b79bb2021a5bf23d64f1710
Michael Mifsud-Sweeney <michaelmifsudsweeney@gmail.com>
2023-01-15 14:39:21 -0500

    add feature to return board game with mandatory comment

--- src/components/ReturnModal/ReturnModal.js
+++ src/components/ReturnModal/ReturnModal.js
@@ -1,26 +1,60 @@
 import React, { useState } from 'react'
 import './ReturnModal.scss'
 import axios from 'axios';
-
+import { UserAuth } from '../../context/AuthContext'
+import uuid4 from 'uuid4';
+import { db } from '../../firebase'
+import { doc, updateDoc, arrayUnion, arrayRemove } from "firebase/firestore";
 function ReturnModal({ selectedGame, setShowModal, loadProfileData }) {
     const [commentText, setCommentText] = useState('');
     const CURRENT_USER_ID = process.env.REACT_APP_CURRENT_USER_ID;
+    const { user } = UserAuth()
 
     const onCommentChange = (e) => setCommentText(e.target.value);
 
-    let returnGameHandler = () => {
-        const API_URL = process.env.REACT_APP_API_URL;
-        let UrlToPostTo = `${API_URL}/games/comment/${selectedGame.gameId}`
+    let returnGameHandler = async () => {
+        if (user && user.uid) {
+            let objectToSend = {
+                "currentUser": user.uid,
+                "commentText": commentText,
+                "datePosted": Date.now(),
+                "commentId": uuid4()
+            }
 
-        let objectToSend = {
-            "currentUser": CURRENT_USER_ID,
-            "commentText": commentText
+            
+            console.log(selectedGame.gameId)
+            const serverRef = doc(db, "games", selectedGame.gameId);
+
+            await updateDoc(serverRef, {
+                gameReviews: arrayUnion(objectToSend)
+            });
+
+            await updateDoc(serverRef, {
+                "gameAvailability": "AVAILABLE",
+                "renterId": ""
+            });
+            
+
         }
 
-        axios.post(UrlToPostTo, objectToSend).then(() => {
-            loadProfileData()
-            setShowModal(false)
-        })
+
+        // const API_URL = process.env.REACT_APP_API_URL;
+        // let UrlToPostTo = `${API_URL}/games/comment/${selectedGame.gameId}`
+
+        
+
+        //add a comment to to the game.
+        //add an object to the comment array
+        //the object will contain the commentors user id, the comment text, a uid. a time of comment
+        //it will also reset the renter's id to nothing and the status to available
+
+
+
+
+        // axios.post(UrlToPostTo, objectToSend).then(() => {
+        //     loadProfileData()
+        //     setShowModal(false)
+        // })
     }
 
     return (
@@ -34,7 +68,7 @@ function ReturnModal({ selectedGame, setShowModal, loadProfileData }) {
                         value={commentText}
                         placeholder="Submit a review"
                         onChange={onCommentChange}
-                        multiline
+                        multiline="true"
                     />
                     <div className="returnModal__button-bar">
                         <button className='returnModal__cancel-button' onClick={() => setShowModal(false)}>Cancel</button>
