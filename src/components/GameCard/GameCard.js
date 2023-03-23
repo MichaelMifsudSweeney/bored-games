9e0a77e0efb9332e14ad351198dc37f30ba3a8b5
Michael Mifsud-Sweeney <michaelmifsudsweeney@gmail.com>
2023-03-23 13:08:03 -0400

    add basic analyics

--- src/components/GameCard/GameCard.js
+++ src/components/GameCard/GameCard.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import "./GameCard.scss"
-
+import { UserAuth } from '../../context/AuthContext'
 function GameCard({ gameData }) {
+  const { logGameClicks } = UserAuth();
   return (
-    <Link to={`/game/${gameData.gameId}`} className="gameCard" >
+    <Link to={`/game/${gameData.gameId}`} className="gameCard" onClick={() => logGameClicks(gameData.gameName)} >
       <div className="gameCard__img-container">
         <div className="gameCard__top-fill"></div>
         <img src={gameData.image} alt="" className='gameCard__img' />
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,10 +1,24 @@
 import {useContext, createContext, useEffect, useState } from 'react'
 import { GoogleAuthProvider, signInWithPopup, signInWithRedirect, signOut, onAuthStateChanged } from "firebase/auth";
 import {auth} from '../firebase'
+import mixpanel from 'mixpanel-browser';
 const AuthContext = createContext()
 
 export const AuthContextProvider = ({ children }) => {
     const [user, setUser] = useState({})
+
+    mixpanel.init('15eda91cba3b7b85aa6cd14ff9c69595', {debug: true}); 
+
+    const logGameClicks = (gameVisit) => {
+        console.log(user)
+        mixpanel.identify(user.uid);
+        let objToSend = { 
+            "Plan": "Premium", 
+            "$name": user.displayName,
+        }
+        objToSend[gameVisit] = "✅";
+        mixpanel.people.set(objToSend);
+    }
 
     const googleSignIn = () => {
         const provider = new GoogleAuthProvider();
@@ -31,7 +45,7 @@ export const AuthContextProvider = ({ children }) => {
     }, [])
 
     return (
-        <AuthContext.Provider value={{ googleSignIn, logOut, user }}>
+        <AuthContext.Provider value={{ googleSignIn, logOut, user, logGameClicks }}>
             {children}
         </AuthContext.Provider>
     )
